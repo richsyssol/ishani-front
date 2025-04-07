41b3bb5d4f99f37bc908c8e4d53c1689f3ebaeda
YatinDevs <c.yatin727@gmail.com>
2025-04-07 12:49:01 +0530

    updates : Our Gallery Section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import OurProducts from "./OurProducts";
+import OurGallery from "./OurGallery";
 
 function Home() {
   return (
     <div>
       <HeroSection />
       <OurProducts />
+      <OurGallery />
     </div>
   );
 }
